4fb1837cdcbd91fda5f09b9794b2de6c030ee405
Brendan Dileo <dileob23@gmail.com>
2025-09-30 09:17:13 -0400

    Update CustomRunnable interface

--- src/main/java/com/brendanddev/threadpool/CustomRunnable.java
+++ src/main/java/com/brendanddev/threadpool/CustomRunnable.java
@@ -1,6 +1,10 @@
 package com.brendanddev.threadpool;
 
-
+/**
+ * A simple interface representing a task that can be executed by 
+ * a thread in a thread pool.
+ */
 public interface CustomRunnable {
 
+    void run();
 }
