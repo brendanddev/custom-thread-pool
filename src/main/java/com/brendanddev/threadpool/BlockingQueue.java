20728b2ac1391432f7837f6e21880a8bd6fd0fb7
Brendan Dileo <dileob23@gmail.com>
2025-09-30 09:19:00 -0400

    Setup BlockingQueue class

--- src/main/java/com/brendanddev/threadpool/BlockingQueue.java
+++ src/main/java/com/brendanddev/threadpool/BlockingQueue.java
@@ -1,5 +1,16 @@
 package com.brendanddev.threadpool;
 
+import java.util.LinkedList;
+
+
 public class BlockingQueue {
+
+    private final List<CustomRunnable> queue;
+    private final int capacity;
+
+    public BlockingQueue(int capacity) {
+        this.capacity = capacity;
+        this.queue = new LinkedList<>();
+    }
     
 }
