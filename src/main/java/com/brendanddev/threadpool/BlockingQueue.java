73636e0f3019e3b8ea5a64e6cf6caa0228cf5986
Brendan Dileo <dileob23@gmail.com>
2025-09-30 09:28:37 -0400

    Implement put method for BlockingQueue

--- src/main/java/com/brendanddev/threadpool/BlockingQueue.java
+++ src/main/java/com/brendanddev/threadpool/BlockingQueue.java
@@ -1,16 +1,40 @@
 package com.brendanddev.threadpool;
 
 import java.util.LinkedList;
+import java.util.List;
 
+import com.brendanddev.threadpool.CustomRunnable;
 
+/**
+ * A thread-safe queue for storing tasks in a thread pool.
+ * It blocks producers when the queue is full and blocks consumers when the queue is empty.
+ */
 public class BlockingQueue {
 
     private final List<CustomRunnable> queue;
     private final int capacity;
 
+    /**
+     * Constructs a new instance of a BlockingQueue with the specified capacity.
+     */
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
         this.queue = new LinkedList<>();
+    }
+
+    /**
+     * Adds a task to the queue. If the queue is full, the calling thread will wait until
+     * space becomes available.
+     * 
+     * @param task The task to add.
+     * @throws InterruptedException If the thread is interruped while waiting.
+     */
+    public synchronized void put(CustomRunnable task) throws InterruptedException {
+        while (queue.size() >= capacity) {
+            wait();
+        }
+        queue.add(task);
+        notifyAll();
     }
     
 }
