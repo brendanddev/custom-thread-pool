e7d5bb072daf284071129dab53a969c57f669425
Brendan Dileo <dileob23@gmail.com>
2025-09-30 15:14:17 -0400

    Update AbortPolicy

--- src/main/java/com/brendanddev/threadpool/AbortPolicy.java
+++ src/main/java/com/brendanddev/threadpool/AbortPolicy.java
@@ -1,8 +1,27 @@
 package com.brendanddev.threadpool;
 
+import com.brendanddev.threadpool.CustomRunnable;
+import com.brendanddev.threadpool.CustomThreadPool;
+import com.brendanddev.threadpool.RejectionPolicy;
 
+import java.util.concurrent.RejectedExecutionException;
 
+/**
+ * A RejectionPolicy that immediately aborts a task submission by throwing a 
+ * RejectedExecutionException when the thread pool is full.
+ */
+public class AbortPolicy implements RejectionPolicy {
 
-public class AbortPolicy {
+    /**
+     * Rejects the given task by throwing a RejectedExecutionException.
+     * 
+     * @param task The task that is being rejected.
+     * @param pool The thread pool attempting to execute the task.
+     * @throws RejectedExecutionException Always thrown to indicate task rejection.
+     */
+    @Override
+    public void reject(CustomRunnable task, CustomThreadPool pool) {
+        throw new RejectedExecutionException("Task " + task + " rejected from " + pool);
+    }
     
 }
