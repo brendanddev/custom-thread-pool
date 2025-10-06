feb0ed9698d57469ca72317a1e2271a77bf74ab7
Brendan Dileo <dileob23@gmail.com>
2025-10-06 16:39:37 -0400

    Setup WorkerThread class

--- src/main/java/com/brendanddev/threadpool/WorkerThread.java
+++ src/main/java/com/brendanddev/threadpool/WorkerThread.java
@@ -1,3 +1,27 @@
-public class WorkerThread {
+package com.brendanddev.threadpool;
+
+import com.brendanddev.threadpool.TaskQueue;
+
+/**
+ * An implementation of a WorkerThread that repeatedly pulls tasks from the shared TaskQueue and executes them.
+ */
+public class WorkerThread extends Thread {
+
+    private final TaskQueue taskQueue;
+    private volatile boolean running = true;
+
+    /**
+     * Constructs a WorkerThread with the specified TaskQueue.
+     * 
+     * @param taskQueue The shared TaskQueue from which this worker will pull tasks.
+     */
+    public WorkerThread(TaskQueue taskQueue) {
+        this.taskQueue = taskQueue;
+    }
+
+    @Override
+    public void run() { }
+
+    public void shutdown() { }
     
 }
