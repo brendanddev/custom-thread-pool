099cc3ac37e0f71d9f68b55044400dfcf2997185
Brendan Dileo <dileob23@gmail.com>
2025-10-07 14:09:28 -0400

    Update RejectionHandler

--- src/main/java/com/brendanddev/threadpool/policies/RejectionHandler.java
+++ src/main/java/com/brendanddev/threadpool/policies/RejectionHandler.java
@@ -1,3 +1,19 @@
-public class RejectionHandler {
+package com.brendanddev.threadpool.policies;
+
+import com.brendanddev.threadpool.CustomThreadPool;
+
+/**
+ * Defines a strategy for handling tasks that cannot be executed by the thread pool.
+ * Allows for custom behaviour when the task queue is fill and no threads are available to execute new tasks.
+ */
+public interface RejectionHandler {
+
+    /**
+     * Handles a task that has been rejected by the thread pool.
+     * 
+     * @param task The Runnable task that was rejected.
+     * @param pool The CustomThreadPool instance that rejected the task.
+     */
+    void reject(Runnable task, CustomThreadPool pool);
     
 }
