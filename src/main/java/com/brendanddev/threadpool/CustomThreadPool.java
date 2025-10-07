c4559eec2b44a293a2e271c3fec22d43077ac9cf
Brendan Dileo <dileob23@gmail.com>
2025-10-07 08:42:31 -0400

    Setup CustomThreadPool class

--- src/main/java/com/brendanddev/threadpool/CustomThreadPool.java
+++ src/main/java/com/brendanddev/threadpool/CustomThreadPool.java
@@ -1,3 +1,29 @@
+package com.brendanddev.threadpool;
+
+
+
 public class CustomThreadPool {
+
+    private final TaskQueue taskQueue;
+    private final WorkerThread[] workers;
+    private volatile boolean isShutdown = false;
+
+    /**
+     * Constructs a CustomThreadPool with a given number of worker threads.
+     * 
+     * @param numThreads The number of worker threads in the pool.
+     */
+    public CustomThreadPool(int numThreads) {
+        taskQueue = new TaskQueue();
+        workers = new WorkerThread[numThreads];
+
+        for (int i = 0; i < numThreads; i++) {
+            workers[i] = new WorkerThread(taskQueue);
+            workers[i].start();
+        }
+    }
+
+    public void execute() { }
+    public void shutdown() { }
     
 }
