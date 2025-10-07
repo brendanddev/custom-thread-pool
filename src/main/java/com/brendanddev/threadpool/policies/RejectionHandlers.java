3fa4a13797f98ab27555d8363c2a4619d4158ce0
Brendan Dileo <dileob23@gmail.com>
2025-10-07 14:27:01 -0400

    Add abort policy

--- src/main/java/com/brendanddev/threadpool/policies/RejectionHandlers.java
+++ src/main/java/com/brendanddev/threadpool/policies/RejectionHandlers.java
@@ -1,3 +1,19 @@
+package com.brendanddev.threadpool.policies;
+
+import com.brendanddev.threadpool.policies.RejectionHandler;
+
+/**
+ * Provides built-in implementations of common rejection handling policies.
+ * Each constants defines a different strategy for what to do when a task cannot be accepted
+ * by the thread pool.
+ */
 public class RejectionHandlers {
+
+    /**
+     * A policy that throws a RuntimeException when a task is rejected.
+     */
+    public static final RejectionHandler ABORT_POLICY = (task, pool) -> {
+        throw new RuntimeException("[ABORT] - Task rejected from " + pool);
+    };
     
 }
